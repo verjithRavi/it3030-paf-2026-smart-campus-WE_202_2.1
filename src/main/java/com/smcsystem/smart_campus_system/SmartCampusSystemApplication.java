fe19b7dcb33f44398debfab86ea07fd6635af1ae
VerjithRavi <raviverjith@gmail.com>
2026-03-30 16:43:00 +0530

    initial spring boot setup with mongodb connection

---
+++ src/main/java/com/smcsystem/smart_campus_system/SmartCampusSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smcsystem.smart_campus_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusSystemApplication.class, args);
+	}
+
+}
